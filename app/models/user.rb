fe22ad09d6728f850fee88982b86e4da131c4380
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-09-24 13:39:56 +0600

    added layout, avatar upload, password reset, email confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def role
+    is_admin ? "ADMIN" : "Normal User"
+  end
+
+  has_one_attached :avatar
 end
