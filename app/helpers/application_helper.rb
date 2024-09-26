61b0aaf994d463ebf9590997dca94ffd760efb32
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-09-26 22:41:33 +0600

    application layout nav will not show in the log in, sign up page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def hide_elements_on_pages?
+    pages_to_hide_elements = [
+      { controller: 'devise/sessions', action: 'new' },
+      { controller: 'registrations', action: 'new' }
+    ]
+
+    # Debugging: Print the current controller and action
+    Rails.logger.debug "Current controller: #{params[:controller]}"
+    Rails.logger.debug "Current action: #{params[:action]}"
+
+    pages_to_hide_elements.any? do |page|
+      params[:controller] == page[:controller] && params[:action] == page[:action]
+    end
+  end
 end
