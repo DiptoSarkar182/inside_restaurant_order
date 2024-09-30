ab249efeda9c3a88a969d3a3c421e35f59a5d9c9
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-09-30 13:08:53 +0600

    added country flag and code for contact number

--- app/javascript/controllers/phone_number_controller.js
+++ app/javascript/controllers/phone_number_controller.js
@@ -1,44 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="phone-number"
 export default class extends Controller {
-  static targets = ["dropdownMenu", "dropdownSpan", "phoneNumberInput", "fullPhoneNumber"];
+  static targets = ["phoneInput"];
 
   connect() {
-    console.log("phone number controller connected")
-    this.updateFullPhoneNumber();
-  }
-
-  toggleDropdown(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    this.dropdownMenuTarget.classList.toggle('hidden');
-  }
-
-  selectCountryCode(event) {
-    if (event.target.tagName === 'LI') {
-      const dataValue = event.target.getAttribute('data-value');
-      this.dropdownSpanTarget.textContent = dataValue;
-      this.dropdownMenuTarget.classList.add('hidden');
-      this.updateFullPhoneNumber();
+    if (this.phoneInputTarget) {
+      this.initializePhoneInput();
     }
   }
 
-  closeDropdown(event) {
-    const isClickInside = this.element.contains(event.target);
-    if (!isClickInside) {
-      this.dropdownMenuTarget.classList.add('hidden');
-    }
-  }
-
-  updatePhoneNumber(event) {
-    event.target.value = event.target.value.replace(/\D/g, '').slice(0, 12);
-    this.updateFullPhoneNumber();
-  }
-
-  updateFullPhoneNumber() {
-    const countryCode = this.dropdownSpanTarget.textContent;
-    const phoneNumber = this.phoneNumberInputTarget.value;
-    this.fullPhoneNumberTarget.value = countryCode + phoneNumber;
+  initializePhoneInput() {
+    intlTelInput(this.phoneInputTarget, {
+      utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
+      separateDialCode: true,
+      initialCountry: "bd",
+      getNumber: true,
+    });
   }
 }
