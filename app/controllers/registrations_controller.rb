181ffa1a7e41c290ac5c87a934e7b0f4e9772671
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-10-01 15:37:47 +0600

    increased width for log in-sign up page, account deletion will show proper notice

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
   # Skip authentication for the registration actions
-  skip_before_action :authenticate_user!, only: [:new, :create]
+  skip_before_action :authenticate_user!, only: [:new, :create, :destroy]
+
+  def destroy
+    Rails.logger.debug "Destroy action called"
+    resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message! :notice, :destroyed
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
 
   private
 
@@ -21,6 +31,13 @@ class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     # Set the flash message
     flash[:notice] = "A confirmation link has been sent to your email address."
+    # Redirect to the login page
+    new_user_session_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    # Set the flash message
+    flash[:notice] = "Your account has been successfully deleted."
     # Redirect to the login page
     new_user_session_path
   end
