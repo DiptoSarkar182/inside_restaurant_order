d121f0e71cce6bc10a207eb3da572355c93db149
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-10-23 18:39:51 +0600

    reworked footer

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="swiper"
 export default class extends Controller {
   connect() {
-    console.log("swiper stimulus connected")
     const swiper = new Swiper(this.element, {
       direction: 'horizontal',
       loop: true,
